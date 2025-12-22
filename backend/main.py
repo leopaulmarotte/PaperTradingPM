d5745a0de62f91de4641525e882fc3640c63ac51
leopaulmarotte <leo-paul.marotte@ensae.fr>
2025-12-22 15:35:11 +0100

    fastapi blank page

---
+++ backend/main.py
@@ -0,0 +1,6 @@
+from fastapi import FastAPI           # Import de FastAPI pour créer l'application web/API
+
+# -----------------------------
+# Création de l'application FastAPI
+# -----------------------------
+app = FastAPI(title="Polystat Arbitrage API")
